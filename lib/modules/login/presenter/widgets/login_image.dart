ea3700cf16f2d48e97f826bc3885a2d7762e8ff1
Malt <malthusim5@gmail.com>
2024-12-04 15:43:54 -0300

    fix image height

--- lib/modules/login/presenter/widgets/login_image.dart
+++ lib/modules/login/presenter/widgets/login_image.dart
@@ -8,16 +8,19 @@ class LoginImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(
-      alignment: Alignment.center,
-      children: [
-        Positioned(top: 20, child: Vector(Vectors.first_wave)),
-        Positioned(top: 108, child: Vector(Vectors.second_wave)),
-        CustomImage(
-          imageUrl: 'assets/images/login.png',
-          local: true,
-        ),
-      ],
+    return const SizedBox(
+      height: 222,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(top: 20, child: Vector(Vectors.first_wave)),
+          Positioned(top: 108, child: Vector(Vectors.second_wave)),
+          CustomImage(
+            imageUrl: 'assets/images/login.png',
+            local: true,
+          ),
+        ],
+      ),
     );
   }
 }
